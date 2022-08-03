79d65def7b0fc730060181e962020f69ae9a1b57
second2050 <git@second2050.me>
2022-08-03 04:07:20 +0200

    moved files around for better organization

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# shellcheck disable=SC2046 # because I actually want splitting
 echo "Compiling..."
-javac me/second2050/raytracer/*.java > compile.log 2>&1
+javac -d ./bin/ $(find src | grep .java) > compile.log 2>&1
 _compileStatus=$?
 if [ $_compileStatus = 0 ]; then
-    java me.second2050.raytracer.RaytracerMain
+    java -classpath ./bin/ me.second2050.raytracer.RaytracerMain
     exit $?
 else
     cat compile.log
